eb82a1aefb65e9ef50b2a2c0620a971c91946f77
Nina <boldurfaith@gmail.com>
2023-04-13 05:47:12 -0700

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,41 @@ console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
+//Exercise 1
+const firstName = "Chika";
+let lastName = "Jonah";
+var age = 16;
 
-// Exercise 1
+//Exercise 2
+lastName = "Mirabel"; //I can not assign a value(firstName) to a constant after initialization
+age = 10;
 
-// Exercise 2
+//Exercise 3
+ let language = "Javascript";
+ let createdYear = 1995;
+ let isCaseSensitive = true;
 
-// Exercise 3
+ console.log(`${langauge} was first released in${createdYear}.`
+ );
+
+ //Exercise 4
+ let product = "Polo shirt"
+ let price = 19.99;
+ let isOnSale = false;
+ let salePercentage = 15;
+ let stock = 8;
+ let inStock = false;
+ let selectedSize = "M";
+
+ console.log(`$The {product} in size ${selectedSize} is $${price} with ${stock} in stock.`
+ );
+
+ //Exercise 5
+ let title = "Name of the Wind";
+ let author = "Patrick Rothfuss";
+ let pageCount = 722;
+ let bookmark = 456;
+ let hasRead = true;
+
+ console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}, which is ${hasRead}.`
+ );
